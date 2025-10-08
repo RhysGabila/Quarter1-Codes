ecd7826881cf5ffca878538933cc8258a1de3375
RhysGabila <rhysfredric.gabila@smc.pshs.edu.ph>
2025-10-08 16:09:20 +0800

    Create SG6_Activity2.py

---
+++ SG6_Activity2.py
@@ -0,0 +1,1 @@
+import math x1 = float(input("Enter x1: ")) y1 = float(input("Enter y1: ")) x2 = float(input("Enter x2: ")) y2 = float(input("Enter y2: ")) pow1 = x2 - x1 pow2 = y2 - y1 d = math.sqrt(math.pow(pow1, 2) + math.pow(pow2, 2)) print("The distance between the two points is ", d)
