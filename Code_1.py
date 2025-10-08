ecb719c3dfbca883413cc51371d1de100524d1e9
RhysGabila <rhysfredric.gabila@smc.pshs.edu.ph>
2025-10-08 16:16:37 +0800

    Create Code_1.py

---
+++ Code_1.py
@@ -0,0 +1,6 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
+sum = num1 + num2 + num2
+
+print(sum)
